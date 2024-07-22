db6313edd9df75ec1f3466b0069f7a897b411a74
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-07-22 10:27:49 +0530

    feat(ui): add Spinner component and documentation

--- apps/www/components/preview/index.tsx
+++ apps/www/components/preview/index.tsx
@@ -7,23 +7,22 @@ const Button = dynamic(() =>
   import('ruru-ui/components/button').then((m) => m.Button),
 );
 
+const Spinner = dynamic(() =>
+  import('ruru-ui/components/spinner').then((m) => m.Spinner),
+);
+
 export default {
-//   card: (
-//     <Wrapper>
-//       <div className="rounded-lg bg-background">
-//         <Card
-//           href="#"
-//           icon={<HomeIcon />}
-//           title="Hello World"
-//           description="Learn More about Caching and Revalidation"
-//         />
-//       </div>
-//     </Wrapper>
-//   ),
   button: (
     <Wrapper>
       <div className="flex items-center justify-center gap-4">
         <Button className='w-fit' variant={"secondary"}> Button </Button>
+      </div>
+    </Wrapper>
+  ),
+  spinner: (
+    <Wrapper>
+      <div className="flex items-center justify-center gap-4">
+        <Spinner size={30} className='my-4' />
       </div>
     </Wrapper>
   ),
