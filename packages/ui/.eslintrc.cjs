a8e2cdce5d5e122eec40e1a1adc752c2ec821e85
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-07-23 22:14:55 +0530

    chore(ci): update CI workflows

--- packages/ui/.eslintrc.cjs
+++ packages/ui/.eslintrc.cjs
@@ -1,4 +1,6 @@
 module.exports = {
+  // Exclude this file from ESLint's list of included files
+  ignorePatterns: [".eslintrc.cjs"],
   plugins: ["@typescript-eslint/eslint-plugin", "eslint-plugin-tsdoc"],
   extends: ["plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
