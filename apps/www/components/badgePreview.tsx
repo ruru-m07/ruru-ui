2ad79ab9fea3ed8f6d6a3ea7cafd5f7b59a22e6c
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-07-25 17:02:07 +0530

    feat(components): add checkbox component

---
+++ apps/www/components/badgePreview.tsx
@@ -0,0 +1,196 @@
+import { CubeIcon } from "@radix-ui/react-icons";
+import { Badge } from "ruru-ui/components/badge";
+
+export const BadgePreview = () => {
+  return (
+    <div className="space-y-2">
+      <div className="space-x-2">
+        <Badge variant="gray" size="lg" icon={<CubeIcon />}>
+          gray
+        </Badge>
+        <Badge variant="gray" size="md" icon={<CubeIcon />}>
+          gray
+        </Badge>
+        <Badge variant="gray" size="sm" icon={<CubeIcon />}>
+          gray
+        </Badge>
+
+        <Badge variant="gray-subtle" size="sm" icon={<CubeIcon />}>
+          gray-subtle
+        </Badge>
+        <Badge variant="gray-subtle" size="md" icon={<CubeIcon />}>
+          gray-subtle
+        </Badge>
+        <Badge variant="gray-subtle" size="lg" icon={<CubeIcon />}>
+          gray-subtle
+        </Badge>
+      </div>
+
+      <div className="space-x-2">
+        <Badge variant="blue" size="lg" icon={<CubeIcon />}>
+          blue
+        </Badge>
+        <Badge variant="blue" size="md" icon={<CubeIcon />}>
+          blue
+        </Badge>
+        <Badge variant="blue" size="sm" icon={<CubeIcon />}>
+          blue
+        </Badge>
+
+        <Badge variant="blue-subtle" size="sm" icon={<CubeIcon />}>
+          blue-subtle
+        </Badge>
+        <Badge variant="blue-subtle" size="md" icon={<CubeIcon />}>
+          blue-subtle
+        </Badge>
+        <Badge variant="blue-subtle" size="lg" icon={<CubeIcon />}>
+          blue-subtle
+        </Badge>
+      </div>
+
+      <div className="space-x-2">
+        <Badge variant="purple" size="lg" icon={<CubeIcon />}>
+          purple
+        </Badge>
+        <Badge variant="purple" size="md" icon={<CubeIcon />}>
+          purple
+        </Badge>
+        <Badge variant="purple" size="sm" icon={<CubeIcon />}>
+          purple
+        </Badge>
+
+        <Badge variant="purple-subtle" size="sm" icon={<CubeIcon />}>
+          purple-subtle
+        </Badge>
+        <Badge variant="purple-subtle" size="md" icon={<CubeIcon />}>
+          purple-subtle
+        </Badge>
+        <Badge variant="purple-subtle" size="lg" icon={<CubeIcon />}>
+          purple-subtle
+        </Badge>
+      </div>
+
+      <div className="space-x-2">
+        <Badge variant="amber" size="lg" icon={<CubeIcon />}>
+          amber
+        </Badge>
+        <Badge variant="amber" size="md" icon={<CubeIcon />}>
+          amber
+        </Badge>
+        <Badge variant="amber" size="sm" icon={<CubeIcon />}>
+          amber
+        </Badge>
+
+        <Badge variant="amber-subtle" size="sm" icon={<CubeIcon />}>
+          amber-subtle
+        </Badge>
+        <Badge variant="amber-subtle" size="md" icon={<CubeIcon />}>
+          amber-subtle
+        </Badge>
+        <Badge variant="amber-subtle" size="lg" icon={<CubeIcon />}>
+          amber-subtle
+        </Badge>
+      </div>
+
+      <div className="space-x-2">
+        <Badge variant="red" size="lg" icon={<CubeIcon />}>
+          red
+        </Badge>
+        <Badge variant="red" size="md" icon={<CubeIcon />}>
+          red
+        </Badge>
+        <Badge variant="red" size="sm" icon={<CubeIcon />}>
+          red
+        </Badge>
+
+        <Badge variant="red-subtle" size="sm" icon={<CubeIcon />}>
+          red-subtle
+        </Badge>
+        <Badge variant="red-subtle" size="md" icon={<CubeIcon />}>
+          red-subtle
+        </Badge>
+        <Badge variant="red-subtle" size="lg" icon={<CubeIcon />}>
+          red-subtle
+        </Badge>
+      </div>
+
+      <div className="space-x-2">
+        <Badge variant="pink" size="lg" icon={<CubeIcon />}>
+          pink
+        </Badge>
+        <Badge variant="pink" size="md" icon={<CubeIcon />}>
+          pink
+        </Badge>
+        <Badge variant="pink" size="sm" icon={<CubeIcon />}>
+          pink
+        </Badge>
+
+        <Badge variant="pink-subtle" size="sm" icon={<CubeIcon />}>
+          pink-subtle
+        </Badge>
+        <Badge variant="pink-subtle" size="md" icon={<CubeIcon />}>
+          pink-subtle
+        </Badge>
+        <Badge variant="pink-subtle" size="lg" icon={<CubeIcon />}>
+          pink-subtle
+        </Badge>
+      </div>
+
+      <div className="space-x-2">
+        <Badge variant="green" size="lg" icon={<CubeIcon />}>
+          green
+        </Badge>
+        <Badge variant="green" size="md" icon={<CubeIcon />}>
+          green
+        </Badge>
+        <Badge variant="green" size="sm" icon={<CubeIcon />}>
+          green
+        </Badge>
+
+        <Badge variant="green-subtle" size="sm" icon={<CubeIcon />}>
+          green-subtle
+        </Badge>
+        <Badge variant="green-subtle" size="md" icon={<CubeIcon />}>
+          green-subtle
+        </Badge>
+        <Badge variant="green-subtle" size="lg" icon={<CubeIcon />}>
+          green-subtle
+        </Badge>
+      </div>
+
+      <div className="space-x-2">
+        <Badge variant="teal" size="lg" icon={<CubeIcon />}>
+          teal
+        </Badge>
+        <Badge variant="teal" size="md" icon={<CubeIcon />}>
+          teal
+        </Badge>
+        <Badge variant="teal" size="sm" icon={<CubeIcon />}>
+          teal
+        </Badge>
+
+        <Badge variant="teal-subtle" size="sm" icon={<CubeIcon />}>
+          teal-subtle
+        </Badge>
+        <Badge variant="teal-subtle" size="md" icon={<CubeIcon />}>
+          teal-subtle
+        </Badge>
+        <Badge variant="teal-subtle" size="lg" icon={<CubeIcon />}>
+          teal-subtle
+        </Badge>
+      </div>
+
+      <div className="space-x-2">
+        <Badge variant="inverted" size="lg" icon={<CubeIcon />}>
+          inverted
+        </Badge>
+        <Badge variant="inverted" size="md" icon={<CubeIcon />}>
+          inverted
+        </Badge>
+        <Badge variant="inverted" size="sm" icon={<CubeIcon />}>
+          inverted
+        </Badge>
+      </div>
+    </div>
+  );
+};
---
+++ packages/ui/src/components/checkbox.tsx
@@ -0,0 +1,80 @@
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+
+import { cn } from "@/utils/cn";
+
+/**
+ * Type definition for Checkbox props, extending the properties of CheckboxPrimitive.Root
+ * and including an optional indeterminate property.
+ */
+type CheckboxProps = React.ComponentPropsWithoutRef<
+  typeof CheckboxPrimitive.Root
+> & {
+  /**
+   * Indicates whether the checkbox is in an indeterminate state.
+   * @default false
+   */
+  indeterminate?: boolean;
+};
+
+/**
+ * Checkbox component wrapping around Radix UI's CheckboxPrimitive.Root.
+ * Provides additional functionality and styling for handling indeterminate state checkboxes.
+ */
+const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  CheckboxProps
+>(({ className, indeterminate = false, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      `peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring ${
+        indeterminate
+          ? "disabled:cursor-default"
+          : "disabled:cursor-not-allowed"
+      } disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground`,
+      className
+    )}
+    disabled={indeterminate}
+    {...props}
+  >
+    {indeterminate ? (
+      <svg
+        width="15"
+        height="15"
+        viewBox="0 0 15 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M2.25 7.5C2.25 7.22386 2.47386 7 2.75 7H12.25C12.5261 7 12.75 7.22386 12.75 7.5C12.75 7.77614 12.5261 8 12.25 8H2.75C2.47386 8 2.25 7.77614 2.25 7.5Z"
+          fill="currentColor"
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    ) : (
+      <CheckboxPrimitive.Indicator
+        className={cn("flex items-center justify-center text-current")}
+      >
+        <svg
+          width="15"
+          height="15"
+          viewBox="0 0 15 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z"
+            fill="currentColor"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </CheckboxPrimitive.Indicator>
+    )}
+  </CheckboxPrimitive.Root>
+));
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export { Checkbox };
