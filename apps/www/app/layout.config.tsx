40baa940abe91ea715be79d1dded74d1f733730f
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-08-08 18:35:36 +0530

    fix(logs): resolve issue with error logs

--- apps/www/app/layout.config.tsx
+++ apps/www/app/layout.config.tsx
@@ -27,6 +27,7 @@ export const baseOptions: BaseLayoutProps = {
       </>
     ),
   },
+  githubUrl: "https://github.com/ruru-m07/ruru-ui/",
   // links: [
   //   {
   //     text: 'Documentation',
--- apps/www/components/EditonGithub.tsx
+++ apps/www/components/EditonGithub.tsx
@@ -9,9 +9,9 @@ const Edit = () => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="size-3 lucide lucide-square-pen"
     >
       <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
