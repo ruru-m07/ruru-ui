7e7a8bd663f2dd3cb447c9d6b77b3eb07d4138fd
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-08-28 19:15:38 +0530

    fix(cli): init directory errors

--- packages/cli/src/utils/index.ts
+++ packages/cli/src/utils/index.ts
@@ -1,13 +1,3 @@
-import path from "path";
-import fs from "fs-extra";
-import { type PackageJson } from "type-fest";
-
-export function getPackageInfo() {
-  const packageJsonPath = path.join("../../package.json");
-
-  return fs.readJSONSync(packageJsonPath) as PackageJson;
-}
-
 export function config() {
   // const baseUrl = "http://localhost:3000";
 
