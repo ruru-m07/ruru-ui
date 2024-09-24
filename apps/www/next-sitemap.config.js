40ffc6eef1c037abba6a8ea34149d68b87f7c635
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-09-24 18:39:44 +0530

    chore(sitemap): update sitemap config and sitemap.xml

--- apps/www/next-sitemap.config.js
+++ apps/www/next-sitemap.config.js
@@ -1,30 +1,14 @@
-const { execSync } = require("child_process");
-
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://ruru-ui.vercel.app",
   generateRobotsTxt: true,
   exclude: ["/api/*"],
   transform: async (config, path) => {
-    const lastmod = await getLastModifiedDate(path);
     return {
       loc: path,
       changefreq: config.changefreq,
       priority: config.priority,
-      lastmod,
       alternateRefs: config.alternateRefs ?? [],
     };
   },
 };
-
-async function getLastModifiedDate(path) {
-  try {
-    const timestamp = execSync(`git log -1 --format=%ct ${path}`)
-      .toString()
-      .trim();
-    return new Date(parseInt(timestamp, 10) * 1000).toISOString();
-  } catch (error) {
-    console.error(`Error getting last modified date for ${path}:`, error);
-    return new Date().toISOString(); // Fallback to current date
-  }
-}
